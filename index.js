40bc46f54d05bbe3c9d11dc1f494a4658cd21efd
Disco Aces <129333044+marbfree@users.noreply.github.com>
2023-06-21 18:16:51 -0400

    added packages inquirer and jest

---
+++ index.js
@@ -0,0 +1,17 @@
+class Shape {
+    // chosen shape, shape color, text color, text
+}
+
+class Triangle {
+    constructor()
+}
+
+class Square {
+    constructor()
+}
+
+class Circle {
+    constructor()
+}
+
+const triangle = new Triangle();
